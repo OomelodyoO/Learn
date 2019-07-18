71c741dbcbd85413d2955b758289a4ae08bef2c1
zhangzhixing <zhangzhixing@boce.cn>
2019-07-18 11:12:40 +0800

    spring-cloud-nacos-user init

---
+++ spring-cloud-nacos-user/src/main/java/win/zhangzhixing/springcloudnacosuser/SpringCloudNacosUserApplication.java
@@ -0,0 +1,15 @@
+package win.zhangzhixing.springcloudnacosuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudNacosUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudNacosUserApplication.class, args);
+    }
+
+}
