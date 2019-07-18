a1ffa8c78b79b4f7cf2566b14cad6cb69476def4
zhangzhixing <zhangzhixing@boce.cn>
2019-07-18 14:35:58 +0800

    nacos user 调用 nacos service

--- spring-cloud-nacos-user/src/main/java/win/zhangzhixing/user/controller/BaseController.java
+++ spring-cloud-nacos-user/src/main/java/win/zhangzhixing/user/controller/BaseController.java
@@ -3,12 +3,20 @@ package win.zhangzhixing.user.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(path = "/base")
 public class BaseController {
+    private final RestTemplate restTemplate;
+
+    public BaseController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping
     public String base() {
-        return "user";
+        String forObject = restTemplate.getForObject("http://service/base/", String.class);
+        return "user" + forObject;
     }
 }
